80882d8ac656b08815f19c6fb4072ab4a9217f89
Murphy573 <429655198@qq.com>
2023-06-15 11:22:29 +0800

    feat: 添加classnames包，提高className书写效率

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,19 @@
 import welcome from '@/assets/images/welcome01.png'
 import HomeStyle from './Home.module.less'
+import classNames from 'classnames'
 
 const Home = () => {
   return (
-    <div className={HomeStyle.home}>
+    <div
+      className={classNames(
+        HomeStyle.home,
+        'a123',
+        {
+          bclass: true,
+          cClass: false,
+        },
+        ['dclass', { eclass: null, fclass: 1 }]
+      )}>
       <img src={welcome} alt="welcome" />
     </div>
   )
